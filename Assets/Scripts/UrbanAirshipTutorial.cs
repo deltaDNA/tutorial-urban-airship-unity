9dc731a3036b7e98895787a4d45adaf3df4fdf30
Laurie McCulloch <laurie.mcculloch@gamesanalytics.com>
2017-12-13 16:37:46 +0000

    deltaDNA Analytics SDK added
    Added deltaDNA analytics SDK

---
+++ Assets/Scripts/UrbanAirshipTutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeltaDNA;
+
+public class UrbanAirshipTutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// Enter additional configuration here
+		DDNA.Instance.ClientVersion = "v1.0.0";
+		DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+
+		// Launch the SDK
+		DDNA.Instance.StartSDK(
+			"79390137655202368653993446815131",
+			"https://collect12606ttrlr.deltadna.net/collect/api",
+			"https://engage12606ttrlr.deltadna.net"
+		);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
