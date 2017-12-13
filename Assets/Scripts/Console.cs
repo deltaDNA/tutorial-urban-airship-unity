ef751e3efda8e4ed12703b066ee37a32c755aa04
Laurie McCulloch <laurie.mcculloch@gamesanalytics.com>
2017-12-13 17:16:12 +0000

    Added debug console to display
    Added debug console to display

---
+++ Assets/Scripts/Console.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UrbanAirshipTutorial
+{
+	public class Console : MonoBehaviour
+	{
+
+
+		public UnityEngine.UI.Text lblConsole;  // displays debug info on deletaDNA SDK and SmartAds
+
+
+		// UI Debug Console will be used to debug what SmartAds and Engage are doing
+		public int numConsoleLines = 12;        // the number of lines that the debug console will be truncated to
+		private List<string> console = new List<string>();
+
+
+		// Use this when the Script is loaded
+		void Awake()
+		{
+			// This will allow us to display Unity Debug.Log in the UI 
+			Application.logMessageReceived += PrintToConsole;
+		}
+		// Use this for initialization
+		void Start()
+		{
+			Debug.Log("Application Started");
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		// Display Debug Messages in UI
+		private void PrintToConsole(string logString, string stackTrace, LogType type)
+		{
+
+			console.Add(string.Format("{0}::{1}\n", System.DateTime.Now.ToString("h:mm:ss tt"), logString));
+			if (console.Count > numConsoleLines)
+			{
+				console.RemoveRange(0, console.Count - numConsoleLines);
+			}
+			lblConsole.text = "";
+			console.ForEach(i => lblConsole.text += i);
+		}
+	}
+}
--- Assets/Scripts/UrbanAirshipTutorial.cs
+++ Assets/Scripts/UrbanAirshipTutorial.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using DeltaDNA;
 
-public class UrbanAirshipTutorial : MonoBehaviour {
+namespace UrbanAirshipTutorial
+{
+	public class UrbanAirshipTutorial : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start () {
 
-		// Enter additional configuration here
-		DDNA.Instance.ClientVersion = "v1.0.0";
-		DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+			// Enter additional configuration here
+			DDNA.Instance.ClientVersion = "v1.0.0";
+			DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
 
-		// Launch the SDK
-		DDNA.Instance.StartSDK(
-			"79390137655202368653993446815131",
-			"https://collect12606ttrlr.deltadna.net/collect/api",
-			"https://engage12606ttrlr.deltadna.net"
-		);
+			// Launch the SDK
+			DDNA.Instance.StartSDK(
+				"79390137655202368653993446815131",
+				"https://collect12606ttrlr.deltadna.net/collect/api",
+				"https://engage12606ttrlr.deltadna.net"
+			);
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		}
 		
+		// Update is called once per frame
+		void Update () {
+			
+		}
 	}
 }
